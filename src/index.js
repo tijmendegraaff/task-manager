aefffa9da4e2c4f61d140aea8dde695d38edcaeb
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-12 19:08:32 +0200

    setup express

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.post("/users", (req, res) => {
+  res.send("testing");
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port:" + port);
+});
