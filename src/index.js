cccf6ebd897e1f4f30a35c1e220a981d56957c10
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-13 15:46:42 +0200

    add get user route

--- src/index.js
+++ src/index.js
@@ -28,7 +28,23 @@ app.get("/users", (req, res) => {
     .then(users => {
       res.send(users);
     })
-    .catch(e => {});
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
 });
 
 app.post("/tasks", (req, res) => {
