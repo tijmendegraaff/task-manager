347ad25a26d34a3fa699690907f1947394ec6bda
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-15 00:27:02 +0200

    add completed query in task route

--- src/routes/task.js
+++ src/routes/task.js
@@ -18,11 +18,23 @@ router.post("/tasks", auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=true
+
 router.get("/tasks", auth, async (req, res) => {
-  const tasks = await Task.find({ owner: req.user._id });
+  const match = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
 
   try {
-    res.send(tasks);
+    await req.user
+      .populate({
+        path: "tasks",
+        match
+      })
+      .execPopulate();
+    res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
   }
