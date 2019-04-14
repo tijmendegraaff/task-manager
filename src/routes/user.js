caad6c538a2e45292d7b1014710bb7d1f64612a0
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-14 12:51:48 +0200

    setup JWT tokens on login and register

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,7 +8,9 @@ router.post("/users", async (req, res) => {
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -20,8 +22,8 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    console.log(user);
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send();
   }
