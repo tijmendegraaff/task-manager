261bbe83d8ccee94c40a2e48f83f2c372c2dce8e
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-14 12:03:27 +0200

    restructure task model for mongoose middleware:

--- src/routes/task.js
+++ src/routes/task.js
@@ -40,7 +40,7 @@ router.get("/tasks/:id", async (req, res) => {
 
 router.patch("/tasks/:id", async (req, res) => {
   const updates = Object.keys(req.body);
-  const allowedUpdates = ["discription", "completed"];
+  const allowedUpdates = ["description", "completed"];
 
   const isValidOperation = updates.every(update => {
     return allowedUpdates.includes(update);
@@ -51,10 +51,13 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
+    const task = await Task.findById(req.params.id);
+
+    updates.forEach(update => {
+      task[update] = req.body[update];
     });
+
+    await task.save();
     if (!task) {
       return res.status(404).send();
     }
