29389c20a8ec19f18ce7695508ed40d0211ce390
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-15 15:17:10 +0200

    setup env keys for dev server

--- src/models/user.js
+++ src/models/user.js
@@ -77,7 +77,7 @@ userSchema.methods.toJSON = function() {
 userSchema.methods.generateAuthToken = async function() {
   const user = this;
 
-  const token = jwt.sign({ _id: user.id.toString() }, "djklfdsjlafjljvkrnwndf");
+  const token = jwt.sign({ _id: user.id.toString() }, process.env.JWT_SECRET);
 
   user.tokens = user.tokens.concat({ token });
 
