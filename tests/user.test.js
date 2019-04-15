04178ad817ce1ea4e5c4b8bb9bd694d0a0271443
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-15 21:05:03 +0200

    finish user tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -111,3 +111,25 @@ test("Should not delete an account for unauthenticated user", async () => {
     .send()
     .expect(401);
 });
+
+test("Should update valid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: "Jess"
+    })
+    .expect(200);
+  const user = await User.findById(userOneId);
+  expect(user.name).toEqual("Jess");
+});
+
+test("Should not update invalid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: "Amsterdam"
+    })
+    .expect(400);
+});
